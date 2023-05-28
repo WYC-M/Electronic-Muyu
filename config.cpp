a0c014b0a8168cec5aafeca69f9f9429c06e422d
WYC-M <miqiwyc@163.com>
2023-05-28 13:26:26 +0800

    23-5-28

--- config.cpp
+++ config.cpp
@@ -12,14 +12,14 @@ void config()
     char configgongde[64];
     
     cout << "=====================================" << endl;
-    cout << "电子木鱼 v2.5 佛祖开光特别版 By WYC-M" << endl;
+    cout << "电子木鱼 v2.6 佛祖开光特别版 By WYC-M" << endl;
     cout << "=====================================" << endl;
 
     cout << endl;
 
-    cout << "设置-------------------------" << endl;
-    cout << "1_自定义文字   2_恢复默认文字" << endl;
-    cout << "-----------------------------" << endl;
+    cout << "设置--------------------------------------------------------------------------------------" << endl;
+    cout << "1_自定义功德文本   2_恢复默认功德文本   3_自定义公告文本   4_恢复默认公告文本   5_清除功德" << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
 
     configpress = _getch();
 
@@ -28,14 +28,14 @@ void config()
     case 49:
     {
         cout << endl;
-        cout << "请输入自定义文字(64字符以内) ";
+        cout << "请输入自定义功德文本(64字符以内) ";
         cin >> configgongde;
 
         // 当没有config_gongde.ini，创建该文件并将内容设置为"功德"
         if (_access(".ewf/config_gongde.ini", 0) == -1)
         {
             ofstream out;
-            out.open(".ewf/score.txt");
+            out.open(".ewf/config_gongde.ini");
             out << "功德";
             out.close();
         }
@@ -55,12 +55,67 @@ void config()
 
     case 50:
     {
+        cout << endl;
         system("del /S /Q .ewf\\config_gongde.ini");
         
         cout << endl;
         cout << "设置完成" << endl;
 
         Sleep(500);
+
+        return;
+    }
+
+    case 51:
+    {
+        cout << endl;
+        cout << "请输入自定义公告文本(64字符以内) ";
+        cin >> configgongde;
+
+        // 当没有text.txt，创建该文件并将内容设置为"功德"
+        if (_access(".ewf/text.txt", 0) == -1)
+        {
+            ofstream out;
+            out.open(".ewf/text.txt");
+            out << "功德";
+            out.close();
+        }
+
+        ofstream out;
+        out.open(".ewf/text.txt");
+        out << configgongde;
+        out.close();
+
+        cout << endl;
+        cout << "设置完成" << endl;
+
+        Sleep(500);
+
+        return;
+    }
+
+    case 52:
+    {
+        cout << endl;
+        system("del /S /Q .ewf\\text.txt");
+
+        cout << endl;
+        cout << "设置完成" << endl;
+
+        Sleep(500);
+
+        return;
+    }
+
+    case 53:
+    {
+        cout << endl;
+        system("del /S /Q .ewf\\score.txt");
+
+        cout << endl;
+        cout << "设置完成,请重启程序" << endl;
+
+        Sleep(1000);
 
         return;
     }
